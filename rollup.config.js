ae49e9f6c8e72f033d7eef095e0aab1f7a5462ef
Markus Felten <Markus.Felten@db.com>
2020-03-10 01:34:31 +0100

    chore: initial checkin

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+import postcss from 'rollup-plugin-postcss'
+import postcssImport from 'postcss-import';
+import tailwindcss from 'tailwindcss';
+
+import dev from "rollup-plugin-dev";
+import { name, description, version, config } from "./package.json";
+
+const production = !process.env.ROLLUP_WATCH;
+const dist = "public";
+const port = 5000;
+
+export default {
+  input: "src/main.mjs",
+  output: {
+    sourcemap: true,
+    format: "esm",
+    file: `${dist}/bundle.mjs`,
+    plugins: [production && terser()]
+  },
+  plugins: [
+    postcss({
+      extract: true,
+      sourcemap: true,
+      minimize: production,
+      plugins: [postcssImport,tailwindcss]
+    }),
+    resolve({ browser: true }),
+    commonjs(),
+    dev({
+      port,
+      dirs: [dist],
+      spa: `${dist}/index.html`,
+      basePath: config.base,
+      proxy: { [`${config.api}/*`]: [config.proxyTarget, { https: true }] }
+    })
+  ],
+  watch: {
+    clearScreen: false
+  }
+};
